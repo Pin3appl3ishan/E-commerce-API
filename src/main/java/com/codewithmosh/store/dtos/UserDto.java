05eeb8ab7f47e9a3ff2bfdb73af24be8fab38001
Pin3appl3ishan <sthaishan2004@gmail.com>
2025-08-13 12:29:43 +0545

    extract query params

--- src/main/java/com/codewithmosh/store/dtos/UserDto.java
+++ src/main/java/com/codewithmosh/store/dtos/UserDto.java
@@ -1,10 +1,16 @@
 package com.codewithmosh.store.dtos;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
+@Setter
 public class UserDto {
     private Long id;
     private String name;
