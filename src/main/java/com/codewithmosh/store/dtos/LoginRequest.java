8ede5af22fe8ebd1b8b68507b10a4ac1b95cb845
Pin3appl3ishan <sthaishan2004@gmail.com>
2025-08-18 15:02:06 +0545

    replace auth logic with AuthManager implementation

---
+++ src/main/java/com/codewithmosh/store/dtos/LoginRequest.java
@@ -0,0 +1,15 @@
+package com.codewithmosh.store.dtos;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class LoginRequest {
+    @NotBlank(message = "Email is required.")
+    @Email
+    private final String email;
+
+    @NotBlank(message = "Password is required.")
+    private final String password;
+}
